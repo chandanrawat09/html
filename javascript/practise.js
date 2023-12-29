79331dd78a6379763645f09ab43a26045434208e
chandan-rawat <rawatchandan1245@gmail.com>
2023-12-29 21:53:47 +0530

    first commet

---
+++ javascript/practise.js
@@ -0,0 +1,24 @@
+//practise codes ;
+//ARTHMETIC OPERATOR:
+
+//let a = 900;
+//let b = 780;
+
+//console.log("a + b = ", a + b);
+//console.log("a - b = ", a - b);
+//console.log("a * b = ", a * b);
+//console.log("a / b = ", a / b);
+
+//MODULUS:
+
+//let a = 9;
+//let b = 7;
+
+//console.log("a % b = ", a % b);
+//console.log("a ** b = ", a ** b);
+
+//incrimination
+//a++;
+//console.log("a++ ", a++);
+//a--;
+//console.log("a--", a--);
